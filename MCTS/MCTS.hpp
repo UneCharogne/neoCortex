2843273a3d80e7a6349326c29be68e2fe6f41b63
UneCharogne <34569885+UneCharogne@users.noreply.github.com>
2017-12-26 20:21:25 +0100

    Update MCTS.hpp

--- MCTS/MCTS.hpp
+++ MCTS/MCTS.hpp
@@ -10,5 +10,25 @@
 
 class MCTS {
   private:
-    Tree tree;
+    Tree tree; //When we initialize, we also need to add the first layer of children
+    Node* currentNode;
+  
+  
+  /*EXPECTED METHODS
+  playMove(state)
+  //Force a move from outside (as an example, played by the opponent). This may be tricky. For instance, when we do this, let's be sure that the following layer of children has been generated, otherwise do that (unless it is a final state)
+  state playBestMove();
+  //Just pick the child with the highest UCT from the current tree's root
+  
+  MCTSsweep
+  
+  Selection
+  //In the selection, we get down picking time by time the highest UCT, until we reach a leaf
+  Expansion
+  //We are now in a leaf. If this leaf has never been visited before, do nothing. Else, if it is a final state, do nothing. Else, expand the tree adding a list of children.
+  Simulation
+  //Do a simulation from the leaf we are in
+  BackPropagation
+  //Backpropagate the reward
+  */
 }
