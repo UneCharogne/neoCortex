cd3ef5fe1ae88a2bd4fc8e472170026f8cd7d915
UneCharogne <34569885+UneCharogne@users.noreply.github.com>
2017-12-26 20:01:46 +0100

    Create MCTS.hpp

---
+++ MCTS/MCTS.hpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <random>
+#include <stdexcept>
+#include <cmath>
+#include <cfloat>
+#include "Game.hpp"
+#include "MCTS.hpp"
+
+
+
+class MCTS {
+  private:
+    Tree tree;
+}
