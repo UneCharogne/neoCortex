7800e237d009f05df537ea6fef8c08e1acae52a6
UneCharogne <34569885+UneCharogne@users.noreply.github.com>
2017-12-29 21:55:20 +0100

    Update Game.hpp

--- MCTS/Game.hpp
+++ MCTS/Game.hpp
@@ -4,7 +4,10 @@
 #include <vector>
 
 
-const std::array<int,64> STARTING_BOARD = {0,1,0,1,0,1,0,1,1,0,1,0,1,0,1,0,0,1,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,-1,0,-1,0,-1,0,0,-1,0,-1,0,-1,0,-1,-1,0,-1,0,-1,0,-1,0};
+typedef std::array<int,64> Board;
+
+
+const Board STARTING_BOARD = {0,1,0,1,0,1,0,1,1,0,1,0,1,0,1,0,0,1,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,-1,0,-1,0,-1,0,0,-1,0,-1,0,-1,0,-1,-1,0,-1,0,-1,0,-1,0};
 
 const std::array<int,32> BLACK_SQUARES = {1,3,5,7,8,10,12,14,17,19,21,23,24,26,28,30,33,35,37,39,40,42,44,46,49,51,53,55,56,58,60,62};
 const std::array<int,2> DIAGONALS = {7,9};
@@ -13,7 +16,7 @@ const std::array<int,2> DIAGONALS = {7,9};
 class GameState {
   private:
     //Array representing the content of the 32 black squares (+/-1 for a white/black draught, +/-2 for a white/black king)
-    std::array<int,64> board; 
+    Board board; 
     
     //Player that has to move (+1 white, -1 black)
     int player;
@@ -23,7 +26,7 @@ class GameState {
     
    public:
     //CONSTRUCTORS
-    GameState(std::array<int,64>, int);
+    GameState(Board, int);
     
     //Standard constructor, gives the starting state
     GameState(void);
