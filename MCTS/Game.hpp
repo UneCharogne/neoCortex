a3551a36a3ee053d7fb06fca0aabe8aa1161c0f6
UneCharogne <34569885+UneCharogne@users.noreply.github.com>
2017-12-29 14:01:48 +0100

    Update Game.hpp

--- MCTS/Game.hpp
+++ MCTS/Game.hpp
@@ -4,11 +4,14 @@
 #include <vector>
 
 
+const std::array<int,32> STARTING_BOARD = {1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+
+
 //Class representing a state of the game
 class GameState {
   private:
     //Array representing the content of the 32 black squares (+/-1 for a white/black draught, +/-2 for a white/black king)
-    int board[32]; 
+    std::array<int,32> board; 
     
     //Player that has to move (+1 white, -1 black)
     int player;
@@ -18,7 +21,7 @@ class GameState {
     
    public:
     //CONSTRUCTORS
-    GameState(int[], int);
+    GameState(std::array<int,32>, int);
     
     //Standard constructor, gives the starting state
     GameState(void);
